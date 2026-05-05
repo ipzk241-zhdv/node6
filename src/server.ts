5389acc3b7438211448e1afaf6cc6f72dc19e9a7
Zherebtsov Dmytro <ipzk241_zhdv@student.ztu.edu.ua>
2026-05-05 16:35:26 +0300

    intentional error

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,8 @@ app.get('/health', (req: Request, res: Response) => {
     // 0 = disconnected, 1 = connected, 2 = connecting, 3 = disconnecting
     const isDbConnected = mongoose.connection.readyState === 1;
 
+    навмисна помилка
+
     if (isDbConnected) {
         res.status(200).json({
             status: 'ok',
